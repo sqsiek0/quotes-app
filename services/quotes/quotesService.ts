2b0f89121938d21d5902f28ec6a4609104610806
Klaudiusz <klaudiusz.kalinowski01@wp.pl>
2025-06-17 12:46:48 +0200

    feat(quotes-api): created quotes model that i am using in api service that exports functions to fetch data

---
+++ services/quotes/quotesService.ts
@@ -0,0 +1,21 @@
+import { QUOTES_RANDOM } from "../../constants/api/Api";
+import { QuoteListResponse, QuoteResponse } from "../../types/Quotes";
+import { apiClient } from "../apiClient";
+
+export async function fetchRandomQuote(): Promise<QuoteResponse> {
+  const { data } = await apiClient.get<QuoteResponse>(QUOTES_RANDOM);
+  return data;
+}
+
+export async function fetchSingleQuoate(id: number): Promise<QuoteResponse> {
+  const { data } = await apiClient.get<QuoteResponse>(`/${id}`);
+  return data;
+}
+
+
+export async function fetchListQuotes(limit = 30, skip = 0): Promise<QuoteListResponse> {
+  const { data } = await apiClient.get<QuoteListResponse>(QUOTES_RANDOM, {
+    params: { limit, skip },
+  });
+  return data;
+}
