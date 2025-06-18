330d1ca8f00f99772786cf0e0ea2ca3584213e85
Klaudiusz <klaudiusz.kalinowski01@wp.pl>
2025-06-18 21:05:05 +0200

    feat(home-data): created hook that is featching random quote and working to handle states on home view - need to test hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,15 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 import { useTheme } from "../../hooks/ThemeProvider";
 import AppButton from "../../components/AppButton";
 import { useRandomQuote } from "../../hooks/quotes/useRandomQuote";
+import { fetchRandomQuote } from "../../services/quotes/quotesService";
+import { useEffect } from "react";
 
 export default function HomeScreen() {
   const placeholderQuote = {
@@ -10,37 +18,64 @@ export default function HomeScreen() {
   };
 
   const [color, typo] = useTheme();
-  const { data, isLoading, isError, error } = useRandomQuote();
+  const { data, isFetching, isLoading, isError, error, refetch } =
+    useRandomQuote();
 
-  if (isLoading) {
-    return (
-      <View style={[styles.container, { backgroundColor: color.background }]}>
-        <Text style={typo.heading}>Loading...</Text>
-      </View>
-    );
-  }
-  if (isError) {
-    return (
-      <View style={[styles.container, { backgroundColor: color.background }]}>
-        <Text style={typo.heading}>Error: {error.message}</Text>
-      </View>
-    );
+  function quoteView() {
+    if (isLoading) {
+      return (
+        <View
+          style={{
+            alignContent: "center",
+            justifyContent: "center",
+            alignSelf: "center",
+          }}
+        >
+          <ActivityIndicator></ActivityIndicator>
+        </View>
+      );
+    }
+    if (isError) {
+      return (
+        <View>
+          <Text style={typo.heading}>Error: {error.message}</Text>
+        </View>
+      );
+    }
+    if (data) {
+      return (
+        <>
+          <View style={styles.quoteContainer}>
+            <Text style={typo.heading}>{data?.quote}</Text>
+          </View>
+          <View style={styles.authorContainer}>
+            <Text style={[typo.small, { fontSize: 18 }]}>
+              - {data?.author || placeholderQuote.author}
+            </Text>
+          </View>
+        </>
+      );
+    }
   }
 
   return (
     <View style={[styles.container, { backgroundColor: color.background }]}>
-      <View style={styles.quoteContainer}>
-        <Text style={typo.heading}>{data?.quote}</Text>
-      </View>
-      <View style={styles.authorContainer}>
-        <Text style={[typo.small, { fontSize: 18 }]}>
-          - {data?.author || placeholderQuote.author}
-        </Text>
-      </View>
-      <View style={styles.buttonRow}>
-        <AppButton title={"New quote"}></AppButton>
+      {/* {quoteView()} */}
+      {isFetching && (
+        <View
+          style={{
+            alignContent: "center",
+            justifyContent: "center",
+            alignSelf: "center",
+          }}
+        >
+          <ActivityIndicator />
+        </View>
+      )}
+      {/* <View style={styles.buttonRow}>
+        <AppButton title={"New quote"} onPress={refetch}></AppButton>
         <AppButton title={"Add to favourite"}></AppButton>
-      </View>
+      </View> */}
     </View>
   );
 }
--- hooks/quotes/useRandomQuote.ts
+++ hooks/quotes/useRandomQuote.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchRandomQuote } from "../../services/quotes/quotesService";
 import type { QuoteResponse } from "../../types/Quotes";
+import { Timer } from "lucide-react-native";
 
 export function useRandomQuote() {
   return useQuery<QuoteResponse, Error>(
@@ -8,6 +9,7 @@ export function useRandomQuote() {
               queryKey: ["randomQuote"],
               queryFn: async () => {
                 const quote = await fetchRandomQuote();
+                await new Promise((resolve) => setTimeout(resolve, 1000));
                 return quote;
               }
          },
