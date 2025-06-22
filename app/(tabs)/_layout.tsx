09eacaacc165b5b6ef1c9126da2188bdb67e2d53
Klaudiusz <klaudiusz.kalinowski01@wp.pl>
2025-06-22 14:56:26 +0200

    fix(pr): deleted commented function and commented wait function in fetchRandomQuote - i will need that for testing

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -18,7 +18,7 @@ type CustomTabBarButtonProps = {
 };
 
 export function createTabBarIcon(
-  routeName: string,
+  routeName: string
 ): (props: {
   focused: boolean;
   color: string;
@@ -70,27 +70,6 @@ export function CustomTabBarButton({
     );
   }
 }
-
-// function getScreenOptions(routeName: string) {
-//   return {
-//     tabBarIcon: createTabBarIcon(routeName),
-//     tabBarActiveTintColor: "#6200ee",
-//     tabBarInactiveTintColor: "#999",
-//     tabBarStyle: {
-//       backgroundColor: "#fff",
-//       borderTopColor: "#ccc",
-//       borderTopWidth: 1,
-//     },
-//     headerTitleAlign: "left",
-//     headerShadowVisible: false,
-//     headerStyle: {
-//       backgroundColor: "#fff",
-//       borderBottomColor: "#ccc",
-//       borderBottomWidth: 1,
-//     },
-//     headerTintColor: "#000",
-//   };
-// }
 
 export default function TabLayout() {
   const [colors, typo] = useTheme();
--- services/quotes/quotesService.ts
+++ services/quotes/quotesService.ts
@@ -5,7 +5,7 @@ import { apiClient } from "../apiClient";
 export async function fetchRandomQuote(): Promise<QuoteResponse> {
   // throw new Error("fetchRandomQuote is not implemented yet");
 
-  await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate network delay
+  // await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate network delay
 
   const { data } = await apiClient.get<QuoteResponse>(QUOTES_RANDOM);
   return data;
