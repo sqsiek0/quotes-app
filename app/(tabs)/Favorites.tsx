b3c59bebe0542b68d61f9e31d8859f33481edc9a
Klaudiusz <klaudiusz.kalinowski01@wp.pl>
2025-06-26 10:22:34 +0200

    feat(favs): added onPress handler to quote component and created list that is showing all favs quotes

--- app/(tabs)/Favorites.tsx
+++ app/(tabs)/Favorites.tsx
@@ -1,9 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { useFavourites } from "../../hooks/useFavourites";
+import { useTheme } from "../../hooks/useTheme";
+import AppQuoteCard from "../../components/AppQuoteCard";
 
 export default function Favorites() {
+  const { state } = useFavourites();
+  const [colors, typo, _, mode] = useTheme();
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Favorites Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: colors.background,
+      }}
+    >
+      <FlatList
+        data={state.ids.map((id) => state.byId[id])}
+        keyExtractor={(item) => item.id.toString()}
+        indicatorStyle={mode === "dark" ? "white" : "black"}
+        initialNumToRender={10}
+        maxToRenderPerBatch={10}
+        removeClippedSubviews
+        style={{ padding: 16 }}
+        renderItem={({ item }) => {
+          return (
+            <AppQuoteCard
+              quote={item.quote}
+              author={item.author}
+              onPress={() => {}}
+            ></AppQuoteCard>
+          );
+        }}
+      ></FlatList>
     </View>
   );
 }
